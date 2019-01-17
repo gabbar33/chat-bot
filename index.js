15c40e1c7809b1009a3e8023c709b597d18d6a88
gabbarsisodiya <gabbar.sisodiya@nagarro.com>
2019-01-17 14:09:21 +0530

    changes port from port to PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const gameDataRoute = require('./modules/routes/gameDataRoutes');
 const playerBetRoute = require('./modules/routes/playerBetRoutes');
